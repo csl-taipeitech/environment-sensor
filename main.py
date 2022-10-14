5e5acb98132f7454fa3a73686ddf326d1a7a62aa
ds94059 <ds94059@gmail.com>
2022-10-14 16:23:06 +0800

    clear code

--- main.py
+++ main.py
@@ -6,7 +6,6 @@ import BME280  # Atmospheric Pressure/Temperature and humidity
 import LTR390  # UV
 import TSL2591  # LIGHT
 import SGP40
-import math
 import json
 
 from machine import Pin, I2C
@@ -45,9 +44,6 @@ def connect_wifi():
         print('Waiting for connection...')
         time.sleep(1)
 
-    if wlan.isconnected():
-        pin16.on()
-
 
 def send_to_server(sensors_dict):
     payload = json.dumps(sensors_dict)
@@ -62,98 +58,6 @@ def send_to_server(sensors_dict):
         print(e)
         print("try again...")
 
-    # path = 'output.txt'
-    # f = open(path, 'a')
-    # f.write(payload)
-    # f.write('\n')
-    # f.close()
-
-
-def get_temp_color(temp):
-    temp_color = ''
-    if temp > -30:
-        temp_color = EscapeCode.Purple
-    if temp > -5:
-        temp_color = EscapeCode.Blue
-    if temp > 5:
-        temp_color = EscapeCode.LightBlue
-    if temp > 15:
-        temp_color = EscapeCode.Green
-    if temp > 27:
-        temp_color = EscapeCode.Orange
-    if temp > 38:
-        temp_color = EscapeCode.Red
-    return temp_color
-
-
-def get_humid_color(humid):
-    humid_color = ''
-    if humid > 0:
-        humid_color = EscapeCode.Red
-    if humid > 40:
-        humid_color = EscapeCode.Green
-    if humid > 70:
-        humid_color = EscapeCode.Blue
-    return humid_color
-
-
-def get_uv_color(uvs):
-    uv_color = ''
-    if uvs >= 0:
-        uv_color = EscapeCode.Green
-    if uvs > 2:
-        uv_color = EscapeCode.Yellow
-    if uvs > 5:
-        uv_color = EscapeCode.Orange
-    if uvs > 7:
-        uv_color = EscapeCode.Red
-    if uvs > 10:
-        uv_color = EscapeCode.Purple
-    return uv_color
-
-
-def print_accel(x, y, z):
-    x = x * 9.81 / 16384
-    y = y * 9.81 / 16384
-    z = z * 9.81 / 16384
-    x_color = EscapeCode.White
-    y_color = EscapeCode.White
-    z_color = EscapeCode.White
-    if abs(x) > 3:
-        x_color = EscapeCode.Red
-    if abs(y) > 3:
-        y_color = EscapeCode.Red
-    if abs(z) > 3:
-        z_color = EscapeCode.Red
-
-    print(f"Acceleration\t:\t{x_color}X = %.2f\t{y_color}Y = %.2f\t{z_color}Z = %.2f\t{EscapeCode.White}m/s2"
-          % (x, y, z))
-
-
-def print_gyro(x, y, z):
-    x /= 131
-    y /= 131
-    z /= 131
-    x_color = EscapeCode.White
-    y_color = EscapeCode.White
-    z_color = EscapeCode.White
-    if abs(x) > 10:
-        x_color = EscapeCode.Yellow
-    if abs(y) > 10:
-        y_color = EscapeCode.Yellow
-    if abs(z) > 10:
-        z_color = EscapeCode.Yellow
-
-    print(f"Gyroscope\t:\t{x_color}X = %.2f\t{y_color}Y = %.2f\t{z_color}Z = %.2f\t{EscapeCode.White}°/sec"
-          % (x, y, z))
-
-
-def print_line():
-    print('------------------------------------------------------------------------------')
-
-
-pin16 = Pin(16, Pin.OUT)
-pin16.off()
 
 print("==================================================")
 print("this is Environment Sensor test program...")
@@ -236,44 +140,15 @@ try:
         if ONLINE:
             send_to_server(sensors_dict)
 
-
-#         print("==================================================")
-        # print(EscapeCode.Clear, end='')
-        # print(EscapeCode.White, end='')
-        # print("pressure\t:\t%.2f hPa" % pressure)
-        # print(f"temp\t\t:\t{get_temp_color(temp)}%.2f °C" % temp)
-        # print(EscapeCode.White, end='')
-        # print(f"hum\t\t:\t{get_humid_color(hum)}%.2f ％" % hum)
-        # print(EscapeCode.White, end='')
-        # print(f"lux\t\t:\t{EscapeCode.Yellow}%d" % lux)
-        # print(EscapeCode.White, end='')
-        # print(f"uv\t\t:\t{get_uv_color(uvs)}%d" % uvs)
-        # print(EscapeCode.White, end='')
-        # print("gas\t\t:\t%.2f" % (32768 - gas))
-        # print_accel(icm[0], icm[1], icm[2])
-        # # print(f"Acceleration\t:\tX = %.2f\tY = %.2f\tZ = %.2f\tm/s2" %
-        # #       (icm[0] * 9.81 / 16384,
-        # #        icm[1] * 9.81 / 16384,
-        # #        icm[2] * 9.81 / 16384))
-        # print_gyro(icm[3], icm[4], icm[5])
-        # # print("Gyroscope\t:\tX = %.2f\tY = %.2f\tZ = %.2f\t°/sec" %
-        # #       (icm[3] / 131,
-        # #        icm[4] / 131,
-        # #        icm[5] / 131))
-        # print("Magnetic\t:\tX = %.2f\tY = %.2f\tZ = %.2f\tµT" %
-        #       (icm[6] / 1000,
-        #        icm[7] / 1000,
-        #        icm[8] / 1000))
-
         print("Pressure:", pressure, end="")
-        print(",temp:", temp, end="")
-        print(",humid:", hum, end="")
-        print(",lux:", lux, end="")
-        print(",uv:", uv, end="")
+        print(",Temp:", temp, end="")
+        print(",Humid:", hum, end="")
+        print(",Lux:", lux, end="")
+        print(",UV:", uv, end="")
         # print(",gas:", 32768-gas, end="")
         print(",Accel X:", icm[0]*9.81/16384, end="")
         print(",Accel Y:", icm[1]*9.81/16384, end="")
-        print(",Accel Z:", icm[2]*9.81/16384, end="")
+        print(",Accel Z:", icm[2]*9.81/16384)
 
         # print(",Gyro X:", icm[3]/131, end="")
         # print(",Gyro Y:", icm[4]/131, end="")
@@ -281,8 +156,7 @@ try:
 
         # print(",Mag X:", icm[6]/1000, end="")
         # print(",Mag Y:", icm[7]/1000, end="")
-        print(",Mag Z:", icm[8]/1000)
+        # print(",Mag Z:", icm[8]/1000)
 
 except KeyboardInterrupt:
-    pin16.off()
     exit()
